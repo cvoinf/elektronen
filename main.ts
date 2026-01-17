a7375d9db187cdadfb5dc91b712b0c72723e07f2
cvoinf <schieder@166315.nrw.schule>
2026-01-17 14:43:15 +0100

    Rand neu berechnet

--- main.ts
+++ main.ts
@@ -2,61 +2,51 @@ namespace SpriteKind {
     export const elektron = SpriteKind.create()
 }
 let l = 0
+let lquadrat = 0
 let dy = 0
 let dx = 0
 let mySprite: Sprite = null
 let list: Sprite[] = []
 list = sprites.allOfKind(SpriteKind.elektron)
+let Reibung = 0.85
+let Abprallfaktor = -0.75
+let HoeheElektronSprite = 0.5
+let BreiteElektronSprite = 0.5
 // Es werden 100 Elektronen erzeugt und in einer Liste abgespeichert
 for (let index = 0; index < 100; index++) {
     mySprite = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 5 4 . . . . . . . 
-        . . . . . . . 4 5 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
+        5 4 
+        4 5 
         `, SpriteKind.elektron)
-    mySprite.setPosition(randint(0, 10), randint(0, 10))
+    mySprite.setPosition(randint(85000, 95000) / 1000, randint(45000, 55000) / 1000)
     mySprite.setVelocity(0, 0)
-    mySprite.setBounceOnWall(true)
-    mySprite.setStayInScreen(true)
+    mySprite.setBounceOnWall(false)
+    mySprite.setStayInScreen(false)
     list.push(mySprite)
 }
 mySprite.setImage(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . 2 2 . . . . . . . . 
-    . . . . . . 2 2 . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    2 2 
+    2 2 
     `)
-controller.moveSprite(mySprite)
-forever(function () {
+game.onUpdateInterval(0.1, function () {
     // Die Reibung sorgt dafür, dass die Elektronen sich nach einiger Zeit beruhigen.
     for (let Wert of list) {
-        Wert.vx = 0.5 * Wert.vx
-        Wert.vy = 0.5 * Wert.vy
+        if (Wert.x > scene.screenWidth() - 10) {
+            Wert.vx = Abprallfaktor * Wert.vx
+            Wert.x = scene.screenWidth() - (10 - BreiteElektronSprite)
+        } else if (Wert.x < 10) {
+            Wert.vx = Abprallfaktor * Wert.vx
+            Wert.x = 10 + BreiteElektronSprite
+        }
+        Wert.vx = Reibung * Wert.vx
+        if (Wert.y > scene.screenHeight() - 10) {
+            Wert.vy = Abprallfaktor * Wert.vy
+            Wert.y = scene.screenHeight() - (10 - HoeheElektronSprite)
+        } else if (Wert.y < 10) {
+            Wert.vy = Abprallfaktor * Wert.vy
+            Wert.y = 10 + HoeheElektronSprite
+        }
+        Wert.vy = Reibung * Wert.vy
     }
     // Jedes Elektron wird durch alle anderen beschleunigt, und zwar in entgegengesetzte Richtung, in Abhängigkeit vom Abstand
     for (let Wert of list) {
@@ -66,14 +56,16 @@ forever(function () {
             if (Wert != Wert2) {
                 dx = Wert.x - Wert2.x
                 dy = Wert.y - Wert2.y
-                l = Math.sqrt(dx * dx + dy * dy)
-                if (Wert.x != Wert2.x) {
-                    Wert.ax += 500 / ((Wert.x - Wert2.x) * (Wert.x - Wert2.x)) * (dx / l)
-                }
-                if (Wert.y != Wert2.y) {
-                    Wert.ay += 500 / ((Wert.y - Wert2.y) * (Wert.y - Wert2.y)) * (dy / l)
+                lquadrat = dx * dx + dy * dy
+                l = Math.sqrt(lquadrat)
+                if (lquadrat != 0) {
+                    Wert.ax += 500 / lquadrat * (dx / l)
+                    Wert.ay += 500 / lquadrat * (dy / l)
                 }
             }
         }
     }
+})
+forever(function () {
+	
 })
